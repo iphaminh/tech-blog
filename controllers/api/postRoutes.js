8f72766ca61cc8ab3ef47f9ae0a64bac79db06a2
iphaminh <126002737+iphaminh@users.noreply.github.com>
2023-08-26 16:28:38 -0400

    update the viewroutes and remove if in postRoutes

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { Post, User } = require('../../models');
+const { Post, User, Comment } = require('../../models');
 
 // GET all posts
 router.get('/', async (req, res) => {
@@ -17,19 +17,29 @@ router.get('/', async (req, res) => {
 router.get('/:id', async (req, res) => {
     try {
         const postData = await Post.findByPk(req.params.id, {
-            include: [{ model: User, attributes: ['username'] }]
+            include: [
+                {
+                    model: User,
+                    attributes: ['username']
+                },
+                {
+                    model: Comment,
+                    include: [User]
+                }
+            ]
         });
 
-        if (!postData) {
-            res.status(404).json({ message: 'No post found with that id!' });
-            return;
-        }
+        const post = postData.get({ plain: true });
 
-        res.status(200).json(postData);
+        res.render('singlepost', { 
+            post,
+            logged_in: req.session.logged_in 
+        });
     } catch (err) {
         res.status(500).json(err);
     }
 });
+
 
 // POST a new post
 router.post('/', async (req, res) => {
