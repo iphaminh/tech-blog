1525ce32530258cc93a930ba43e0a378cb2086d1
iphaminh <126002737+iphaminh@users.noreply.github.com>
2023-08-26 01:15:40 -0400

    set up server and apiuserrouter for signup...

--- server.js
+++ server.js
@@ -2,6 +2,10 @@ const sequelize = require('./config/connection');
 
 // ... other server setup code ...
 
+sequelize.sync({ force: false }).then(() => { //option ensures that the tables are not recreated if they already exist. If you set it to true, it will drop and recreate the tables every time you start the server.
+  app.listen(PORT, () => console.log('Now listening'));
+});
+
 sequelize.authenticate()
   .then(() => {
     console.log('Database connected successfully.');
