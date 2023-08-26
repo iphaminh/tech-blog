4dce7f94ba3fa9ab491deb8b358c672a090648ea
iphaminh <126002737+iphaminh@users.noreply.github.com>
2023-08-26 13:36:17 -0400

    update server fix error handlebars n the template

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const sequelize = require('./config/connection');
-const exphbs = require('express-handlebars');
+const exphbs = require('express-handlebars').engine;
+
 
 const app = express();
 
