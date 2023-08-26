5f43fe00cbcc824032a6a6ab92561bbdbe6542da
iphaminh <126002737+iphaminh@users.noreply.github.com>
2023-08-26 13:00:28 -0400

    update the display for specific post /:id

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,10 +1,30 @@
 const router = require('express').Router();
-const { Post } = require('../../models');
+const { Post, User } = require('../../models');
 
+// Route for the homepage to display all posts
 router.get('/', async (req, res) => {
     try {
         const postData = await Post.findAll();
         res.render('homepage', { posts: postData });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+// Route to view a single post based on its ID
+router.get('/post/:id', async (req, res) => {
+    try {
+        const postData = await Post.findByPk(req.params.id, {
+            include: [{ model: User, attributes: ['username'] }]
+        });
+
+        if (!postData) {
+            res.status(404).json({ message: 'No post found with this id!' });
+            return;
+        }
+
+        // Render the singlepost template with the post data
+        res.render('singlepost', { post: postData });
     } catch (err) {
         res.status(500).json(err);
     }
