2a86da5f295f8a8b8b04ceeb6ce2eaa06491f913
iphaminh <126002737+iphaminh@users.noreply.github.com>
2023-08-26 10:39:35 -0400

    create  and setup CRUD operations for postR

---
+++ controllers/api/postRoutes.js
@@ -0,0 +1,84 @@
+const router = require('express').Router();
+const { Post, User } = require('../../models');
+
+// GET all posts
+router.get('/', async (req, res) => {
+    try {
+        const postData = await Post.findAll({
+            include: [{ model: User, attributes: ['username'] }]
+        });
+        res.status(200).json(postData);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+// GET a single post by id
+router.get('/:id', async (req, res) => {
+    try {
+        const postData = await Post.findByPk(req.params.id, {
+            include: [{ model: User, attributes: ['username'] }]
+        });
+
+        if (!postData) {
+            res.status(404).json({ message: 'No post found with that id!' });
+            return;
+        }
+
+        res.status(200).json(postData);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+// POST a new post
+router.post('/', async (req, res) => {
+    try {
+        const postData = await Post.create(req.body);
+        res.status(200).json(postData);
+    } catch (err) {
+        res.status(400).json(err);
+    }
+});
+
+// PUT update a post
+router.put('/:id', async (req, res) => {
+    try {
+        const postData = await Post.update(req.body, {
+            where: {
+                id: req.params.id,
+            },
+        });
+
+        if (!postData) {
+            res.status(404).json({ message: 'No post found with that id!' });
+            return;
+        }
+
+        res.status(200).json(postData);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+// DELETE a post
+router.delete('/:id', async (req, res) => {
+    try {
+        const postData = await Post.destroy({
+            where: {
+                id: req.params.id,
+            },
+        });
+
+        if (!postData) {
+            res.status(404).json({ message: 'No post found with that id!' });
+            return;
+        }
+
+        res.status(200).json(postData);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+module.exports = router;
