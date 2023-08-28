df6850866c8858a59e0a5e0cf2c5cd312cac804a
iphaminh <126002737+iphaminh@users.noreply.github.com>
2023-08-28 16:48:48 -0400

    update dashbaord on path

--- controllers/dashboardRoutes.js
+++ controllers/dashboardRoutes.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const { Post, User } = require('../models');
-const withAuth = require('../../utils/auth');
+const withAuth = require('../utils/auth');
 
 
 // GET route to display the user's dashboard with their posts
