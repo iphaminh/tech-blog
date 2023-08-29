1dccd20c2948a916397e5e5d1719288fd4e1c511
iphaminh <126002737+iphaminh@users.noreply.github.com>
2023-08-29 18:14:53 -0400

    update files

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,12 +1,8 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
-const postRoutes = require('./postRoutes');
-const commentRoutes = require('./commentRoutes');
-
-
+const projectRoutes = require('./projectRoutes');
 
 router.use('/users', userRoutes);
-router.use('/posts', postRoutes);
-router.use('/comments', commentRoutes);
+router.use('/projects', projectRoutes);
 
 module.exports = router;
