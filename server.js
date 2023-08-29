6c6b3935d925e625d7df97506b54dc00039e003d
iphaminh <126002737+iphaminh@users.noreply.github.com>
2023-08-29 15:18:41 -0400

    add index

--- server.js
+++ server.js
@@ -39,6 +39,11 @@ app.use(session(sess));
 // Inform Express.js on which template engine to use
 app.engine('handlebars', hbs.engine);  // Make sure this line comes after `hbs` is defined
 app.set('view engine', 'handlebars');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
 // Import the routes
 // const homeRoutes = require('./controllers/homeRoutes');
 // const dashboardRoutes = require('./controllers/dashboardRoutes');
