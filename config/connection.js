ceeef3b138a9b23b09c83af412ac5c4123138252
iphaminh <126002737+iphaminh@users.noreply.github.com>
2023-08-29 14:15:05 -0400

    fix sql

--- config/connection.js
+++ config/connection.js
@@ -2,7 +2,7 @@ const Sequelize = require('sequelize');
 require('dotenv').config();
 
 // Create a Sequelize connection to the database using the URL in .env
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
     host: '127.0.0.1',  // Explicitly set host to IPv4 address
     dialect: 'mysql',
     port: 3306
